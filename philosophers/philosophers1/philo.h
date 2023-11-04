bb09145394301ee01347f4012980e074c89a2092
Haruto Miyazaki <hmiyazak@c1r15s8.42tokyo.jp>
2023-11-04 14:52:40 +0900

    init philo

---
+++ philosophers/philosophers1/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmiyazak <hmiyazak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 14:39:25 by hmiyazak          #+#    #+#             */
+/*   Updated: 2023/11/04 14:44:16 by hmiyazak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
