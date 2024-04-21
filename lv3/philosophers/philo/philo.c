e0ad31c5c6c408de2d57d9f769db883ed70a2e3d
Haruto Miyazaki <hmiyazak@c1r4s3.42tokyo.jp>
2024-04-21 17:29:54 +0900

    drastic change in 42 log

---
+++ lv3/philosophers/philo/philo.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmiyazak <hmiyazak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 21:08:16 by hmiyazak          #+#    #+#             */
+/*   Updated: 2023/12/18 16:46:21 by hmiyazak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static void	*philo_action(void *philo);
+static int	philo_cycle(t_philo *philo, t_table *table);
+void		join_philos(t_philo *philos, int philo_num);
+
+int	philo_simulation(t_table *table)
+{
+	int		iter;
+	t_philo	*actor;
+
+	iter = 0;
+	if (table == NULL || table->philos == NULL)
+		return (-1);
+	while (iter < table->philo_num)
+	{
+		actor = &(table->philos[iter]);
+		actor->philo_id = iter + 1;
+		actor->last_eat = table->start_time;
+		if (pthread_create(actor->where, NULL, philo_action, actor) != 0)
+		{
+			if (pthread_mutex_lock(&(table->alive_mutex)) == 0)
+				table->all_alive = 0;
+			join_philos(table->philos, iter);
+			return (-1);
+		}
+		usleep(10);
+		iter += 1;
+	}
+	return (0);
+}
+
+static void	*philo_action(void *philo)
+{
+	int			iter;
+	long int	cycle;
+	t_philo		*actor;
+	t_table		*table;
+
+	iter = 0;
+	actor = philo;
+	table = actor->table;
+	if (table->num_eat < 0)
+		cycle = LONG_MAX;
+	else
+		cycle = table->num_eat;
+	while (iter < cycle)
+	{
+		if (philo_cycle(actor, table) == 1)
+			break ;
+		iter += 1;
+	}
+	return (NULL);
+}
+
+static int	philo_cycle(t_philo *philo, t_table *table)
+{
+	if (take_fork(philo, table) != 0)
+		return (1);
+	if (start_eating(philo, table) != 0)
+		return (1);
+	if (start_sleeping(philo, table) != 0)
+		return (1);
+	if (start_thinking(philo, table) != 0)
+		return (1);
+	return (0);
+}
+
+void	join_philos(t_philo *philos, int philo_num)
+{
+	int	iter;
+
+	if (philos == NULL)
+		return ;
+	iter = 0;
+	while (iter < philo_num)
+	{
+		if (philos[iter].where != NULL)
+		{
+			pthread_join(*(philos[iter].where), NULL);
+			free(philos[iter].where);
+		}
+		iter += 1;
+	}
+	free(philos);
+}
