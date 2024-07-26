088cfe34385833a2e48a922c991b1a7940089348
Haruto Miyazaki <hmiyazak@c4r3s3.42tokyo.jp>
2024-07-26 10:14:42 +0900

    done creating flame

---
+++ cpp/cpp02/ex01/Fixed.cpp
@@ -0,0 +1,53 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) {
+    std::cout << "Default constructor called" << std::endl;
+    this->_value = 0;
+}
+
+Fixed::Fixed(const Fixed &object) {
+    std::cout << "Copy constructor called" << std::endl;
+    this->_value = object.getRawBits();
+}
+
+Fixed::Fixed(const int value) {
+    std::cout << "Int constructor called" << std::endl;
+    _value = value << _fractional;
+}
+
+Fixed::Fixed(const float value) {
+    std::cout << "Float constructor called" << std::endl;
+    _value = value;
+}
+
+Fixed& Fixed::operator = (const Fixed &object) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->_value = object.getRawBits();
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& object) {
+    out << object.toFloat();
+    return out;
+}
+
+Fixed::~Fixed(void) {
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_value;
+}
+void Fixed::setRawBits(int const raw) {
+    this->_value = raw << _fractional;
+}
+
+float Fixed::toFloat(void) const {
+    float float_value = static_cast<float>(_value) / (1 << _fractional);
+    return float_value;
+}
+
+int Fixed::toInt(void) const {
+    return _value >> _fractional;
+}
---
+++ cpp/cpp02/ex01/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <string>
+# include <iostream>
+# include <cmath>
+
+class Fixed {
+private:
+    int _value;
+    static const int _fractional = 8;
+public:
+    Fixed(void);
+    Fixed(const Fixed &object);
+    Fixed(const int value);
+    Fixed(const float value);
+    Fixed& operator = (const Fixed &object);
+    ~Fixed(void);
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+    float toFloat(void) const;
+    int toInt(void) const;
+};
+
+std::ostream& operator<<(std::ostream& out, const Fixed& object);
+
+#endif
