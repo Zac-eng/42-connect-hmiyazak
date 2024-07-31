ea7888055a417042ff10995528e9d37a8046a51e
Haruto Miyazaki <hmiyazak@c3r3s8.42tokyo.jp>
2024-07-31 11:01:16 +0900

    done ex01

---
+++ cpp/cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef SCAV_TRAP_HPP
+# define SCAV_TRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap: public ClapTrap {
+
+public:
+    ScavTrap(void);
+    ScavTrap(ScavTrap& object);
+    ScavTrap(std::string init_name);
+    ~ScavTrap();
+    ScavTrap& operator = (const ScavTrap& object);
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);   
+    void beRepaired(unsigned int amount);
+    void guardGate();
+};
+
+#endif
